3cfc3f11eaaf1d6f0ba8a14034d14cd9cac477ab
Ben Askren <ben@buildcalc.com>
2017-07-03 17:28:43 -0400

    Added AsyncAwaitForSet

--- PCL.Utils/AsyncAwaitForSet.cs
+++ PCL.Utils/AsyncAwaitForSet.cs
@@ -1,10 +1,48 @@
 ﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
 namespace PCL.Utils
 {
-    public class AsyncAwaitForSet
+    public class AsyncAwaitForSet<T>
     {
-        public AsyncAwaitForSet()
+        public AsyncAwaitForSet(Action onStartAction, Func<T, bool> onSetAction)
         {
+            _onSetAction = onSetAction;
+            onStartAction?.Invoke();
         }
+
+        #region Async Constructor / Presenter / Result
+        volatile TaskCompletionSource<T> m_tcs = new TaskCompletionSource<T>();
+
+        AsyncAwaitForSet<T> CurrentInstance;
+        Func<T, bool> _onSetAction;
+
+        public Task<T> Result()
+        {
+            return m_tcs.Task;
+        }
+
+
+        public void Set(T result)
+        {
+            if (_onSetAction != null && !_onSetAction.Invoke(result))
+                return;
+            m_tcs.TrySetResult(result);
+            CurrentInstance = null;
+        }
+
+        public void Reset()
+        {
+            while (true)
+            {
+                var tcs = m_tcs;
+                if (!tcs.Task.IsCompleted ||
+                    Interlocked.CompareExchange(ref m_tcs, new TaskCompletionSource<T>(), tcs) == tcs)
+                    return;
+            }
+        }
+        #endregion
+
     }
 }
