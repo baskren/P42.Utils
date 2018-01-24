d779ceb85eb29dc4934cccfcf20dc9a5b332a6be
Ben Askren <ben@buildcalc.com>
2018-01-24 10:43:17 -0500

    Got rid of PCL implementations;

--- P42.Utils.iOS/Properties/AssemblyInfo.cs
+++ P42.Utils.iOS/Properties/AssemblyInfo.cs
@@ -1,20 +1,26 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-using Android.App;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("P42.Utils.NetStandard.Droid")]
+[assembly: AssemblyTitle("P42.Utils.iOS")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("P42.Utils.NetStandard.Droid")]
+[assembly: AssemblyProduct("P42.Utils.iOS")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("530e21ba-a725-4714-8678-74cca5cdbb38")]
 
 // Version information for an assembly consists of the following four values:
 //
