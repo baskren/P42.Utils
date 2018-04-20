2c6dc0b8df7c3aef0b790c897fe06c534f70d0fa
DESKTOP-SO17ACK\ben <ben@buildcalc.com>
2018-04-20 15:49:36 -0400

    Added P42.Utils.UWP; Moved PlatformPathLoader from FormsGestures to P42.Utils.UWP

---
+++ P42.Utils.UWP/Settings.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P42.Utils.UWP
+{
+    public class Settings
+    {
+        public static Windows.UI.Xaml.Application Application { get; private set; }
+
+        public static void Init(Windows.UI.Xaml.Application application)
+        {
+            Application = application;
+            P42.Utils.Environment.Init();
+            P42.Utils.Environment.PlatformPathLoader = PlatformPathLoader;
+        }
+
+        static void PlatformPathLoader()
+        {
+            /*
+            //var envVars = System.Environment.GetEnvironmentVariables();
+
+            try
+            {
+                //var documentsFolderPath = Windows.Storage.KnownFolders.DocumentsLibrary?.Path;
+                //P42.Utils.Environment.DocumentsPath = documentsFolderPath;//Windows.Storage.ApplicationData.Current.LocalFolder.Path; 
+            }
+            catch (System.UnauthorizedAccessException)
+            {
+                // we don't have access.  Oh well.
+            }
+            */
+            P42.Utils.Environment.ApplicationDataPath = Windows.Storage.ApplicationData.Current.LocalFolder.Path;
+            P42.Utils.Environment.ApplicationCachePath = Windows.Storage.ApplicationData.Current.LocalCacheFolder.Path;
+            P42.Utils.Environment.TemporaryStoragePath = Windows.Storage.ApplicationData.Current.TemporaryFolder.Path;
+        }
+    }
+}
