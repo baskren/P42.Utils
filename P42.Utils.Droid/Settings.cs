2fb0ada06819ea132fb027644fd2b2b306c1a4a3
Ben Askren <ben@buildcalc.com>
2018-04-20 12:27:00 -0400

    Moved platform path initialization from FormsGestures to P42.Utils.platform projects

---
+++ P42.Utils.Droid/Settings.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace P42.Utils.Droid
+{
+    public static class Settings
+    {
+        public static Android.Content.Context Context { get; private set; }
+
+
+        public static void Init(Android.Content.Context context)
+        {
+            Context = context;
+            P42.Utils.Environment.Init();
+            P42.Utils.Environment.PlatformPathLoader = PlatformPathLoader;
+        }
+
+        static void PlatformPathLoader()
+        {
+            //P42.Utils.Environment.DocumentsPath = Context.FilesDir.Path;
+            P42.Utils.Environment.ApplicationDataPath = System.IO.Path.Combine(Context.ApplicationInfo.DataDir, "AppData");
+            P42.Utils.Environment.ApplicationCachePath = Context.CacheDir.Path;
+            P42.Utils.Environment.TemporaryStoragePath = System.IO.Path.Combine(Context.CacheDir.Path, "tmp");
+        }
+    }
+}
---
+++ P42.Utils.iOS/Settings.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Foundation;
+
+namespace P42.Utils.iOS
+{
+    public static class Settings
+    {
+        public static void Init()
+        {
+            P42.Utils.Environment.Init();
+            P42.Utils.Environment.PlatformPathLoader = PlatformPathLoader;
+        }
+
+        static void PlatformPathLoader()
+        {
+            var nsError = new NSError();
+            //var documentsDir = NSFileManager.DefaultManager.GetUrl(NSSearchPathDirectory.DocumentDirectory, NSSearchPathDomain.User, null, true, out nsError);
+            //if (nsError == null && !string.IsNullOrWhiteSpace(documentsDir?.Path))
+            //    P42.Utils.Environment.DocumentsPath = documentsDir.Path;
+            //else
+            //    throw new Exception("Could not get iOS Documents Directory");
+
+            var appSupportDir = NSFileManager.DefaultManager.GetUrl(NSSearchPathDirectory.ApplicationSupportDirectory, NSSearchPathDomain.User, null, true, out nsError);
+            if (nsError == null && !string.IsNullOrWhiteSpace(appSupportDir?.Path))
+                P42.Utils.Environment.ApplicationDataPath = System.IO.Path.Combine(appSupportDir.Path, NSBundle.MainBundle.BundleIdentifier);
+            else
+                throw new Exception("Could not get iOS Application Support Directory");
+
+
+            var cacheDir = NSFileManager.DefaultManager.GetUrl(NSSearchPathDirectory.CachesDirectory, NSSearchPathDomain.User, null, true, out nsError);
+            if (nsError == null && !string.IsNullOrWhiteSpace(cacheDir?.Path))
+                P42.Utils.Environment.ApplicationCachePath = System.IO.Path.Combine(cacheDir.Path, NSBundle.MainBundle.BundleIdentifier);
+            else
+                throw new Exception("Could not get iOS Cache Directory");
+
+            // either of the following seems to work
+            //var tmpDirPath = NSFileManager.DefaultManager.GetTemporaryDirectory()?.Path;
+            var tmpDirPath = System.IO.Path.GetTempPath();
+            P42.Utils.Environment.TemporaryStoragePath = tmpDirPath;
+
+
+            var current = NSFileManager.DefaultManager.CurrentDirectory;
+        }
+
+        [System.Runtime.InteropServices.DllImport(ObjCRuntime.Constants.FoundationLibrary)]
+        public static extern IntPtr NSHomeDirectory();
+
+        static string _appDirectory;
+        public static string AppDirectory
+        {
+            get
+            {
+                _appDirectory = _appDirectory ?? ((NSString)ObjCRuntime.Runtime.GetNSObject(NSHomeDirectory())).ToString();
+                return _appDirectory;
+            }
+        }
+
+
+    }
+}
