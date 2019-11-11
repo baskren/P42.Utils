5edbfb05116d6fd3a4109f9399d6a380c372177a
Ben Askren <ben@buildcalc.com>
2019-11-11 10:29:29 -0500

    Fixed: - Android DiskSpace crash;

--- P42.Utils.Droid/DiskSpace.cs
+++ P42.Utils.Droid/DiskSpace.cs
@@ -8,9 +8,9 @@ namespace P42.Utils.Droid
     {
         static Java.IO.File[] FileRoots => P42.Utils.Droid.Settings.Context.GetExternalFilesDirs(null);
 
-        public static string PhoneRootPath => FileRoots[0].AbsolutePath; // PhoneMemory
-        public static string SDCardPath = FileRoots[1].AbsolutePath; // SCCard (if available)
-        public static string UsbPath = FileRoots[2].AbsolutePath; // USB (if available)
+        public static string PhoneRootPath => FileRoots.Length > 0 ? FileRoots[0].AbsolutePath : null; // PhoneMemory
+        //public static string SDCardPath = FileRoots.Length > 1 ? FileRoots[1].AbsolutePath : null; // SCCard (if available)
+        //public static string UsbPath = FileRoots.Length > 2 ? FileRoots[2].AbsolutePath : null; // USB (if available)
 
         public ulong Free => FreeMemory(PhoneRootPath);
 
@@ -21,6 +21,8 @@ namespace P42.Utils.Droid
 
         public static ulong TotalMemory(string path)
         {
+            if (string.IsNullOrWhiteSpace(path))
+                return 0;
             using (StatFs statFs = new StatFs(path))
             {
                 if (Build.VERSION.SdkInt < Android.OS.BuildVersionCodes.JellyBeanMr2) //Build.VERSION_CODES.JellyBeanMr2)
@@ -32,6 +34,8 @@ namespace P42.Utils.Droid
 
         public static ulong FreeMemory(string path)
         {
+            if (string.IsNullOrWhiteSpace(path))
+                return 0;
             using (StatFs statFs = new StatFs(path))
             {
                 if (Build.VERSION.SdkInt < Android.OS.BuildVersionCodes.JellyBeanMr2) //Build.VERSION_CODES.JellyBeanMr2)
