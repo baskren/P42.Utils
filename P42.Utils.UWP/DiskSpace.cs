90c48c868aad067f8230b11e5b01cebf75eca15f
baskren <ben@buildcalc.com>
2019-11-11 08:37:05 -0500

    UWP implementation of DiskSpace

--- P42.Utils.UWP/DiskSpace.cs
+++ P42.Utils.UWP/DiskSpace.cs
@@ -1,29 +1,57 @@
-﻿using System;
+﻿using P42.Utils.Interfaces;
+using System;
 using System.Threading.Tasks;
-
+using Windows.Storage;
 
 namespace P42.Utils.UWP
 {
-	public class DiskSpace : IDiskSpace
-	{
-		public ulong Free
-		{
-			get
-			{
-				var task = GetFreeSpace();
-				var result = task.WaitAndUnwrapException();
-				return result;
-			}
-		}
+    public class DiskSpace : IDiskSpace
+    {
+        public ulong Free
+        {
+            get
+            {
+                var task = GetFreeSpace();
+                var waiter = task.GetAwaiter();
+                var result = waiter.GetResult();
+                return result;
+            }
+        }
+
+        public ulong Size
+        {
+            get
+            {
+                var task = GetCapacity();
+                var waiter = task.GetAwaiter();
+                var result = waiter.GetResult();
+                return result;
+            }
+        }
+
+        public ulong Used
+        {
+            get
+            {
+                return Size - Free;
+            }
+        }
+
+        async Task<UInt64> Get(string property)
+        {
+            StorageFolder local = ApplicationData.Current.LocalFolder;
+            var task = local.Properties.RetrievePropertiesAsync(new string[] { property });
+            var retrivedProperties = await task.AsTask().ConfigureAwait(false);
+            return (UInt64)retrivedProperties[property];
+        }
+
+        async Task<UInt64> GetFreeSpace()
+            => await Get("System.FreeSpace").ConfigureAwait(false);
+
+        async Task<UInt64> GetCapacity()
+            => await Get("System.Capacity").ConfigureAwait(false);
 
 
-		public async Task<UInt64> GetFreeSpace()
-		{
-			StorageFolder local = ApplicationData.Current.LocalFolder;
-			var retrivedProperties = await local.Properties.RetrievePropertiesAsync(new string[] { "System.FreeSpace" }).ConfigureAwait(false);
-			return (UInt64)retrivedProperties["System.FreeSpace"];
-		}
 
-
-	}
+    }
 }
