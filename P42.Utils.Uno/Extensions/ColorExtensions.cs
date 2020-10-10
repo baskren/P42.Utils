9463e869411b49372bad19263e1a7b507998b090
Ben Askren <ben@buildcalc.com>
2020-10-10 14:42:55 -0400

    Updated Uno.UI; Added ColorExtensions;

---
+++ P42.Utils.Uno/Extensions/ColorExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI;
+using Windows.UI.Xaml.Media;
+
+namespace P42.Utils.Uno
+{
+    public static class ColorExtensions
+    {
+        public static SolidColorBrush ToBrush(this Color color)
+            => new SolidColorBrush(color);
+    }
+}
