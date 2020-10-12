38299631d118ca4a390911c97f1d999fe84ef71c
Ben Askren <ben@buildcalc.com>
2020-10-12 10:41:15 -0400

    Added: - VisibilityExtensions; - AppInfo;

---
+++ P42.Utils.Uno/AppInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel;
+
+namespace P42.Utils.Uno
+{
+    public static class AppInfo
+    {
+        public static string Version => Package.Current.Id.Version.Major + "." + Package.Current.Id.Version.Minor; // + "." + Package.Current.Id.Version.Revision;
+
+        public static int Build => Package.Current.Id.Version.Build;
+
+        public static string Identifier => Package.Current.Id.ResourceId;
+
+        public static string Name => Package.Current.DisplayName;
+    }
+}
---
+++ P42.Utils.Uno/Extensions/VisibilityExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Uno.Extensions.Specialized;
+using Windows.UI.Xaml;
+
+namespace P42.Utils.Uno
+{
+    public static class VisibilityExtensions
+    {
+        public static Visibility ToVisibility(this bool value)
+            => value ? Visibility.Visible : Visibility.Collapsed;
+
+        public static Visibility ToVisibility(this IEnumerable collection)
+            => collection?.Any() ?? false
+                    ? Visibility.Visible
+                    : Visibility.Collapsed;
+
+        public static bool ToBool(this Visibility visibility)
+            => visibility == Visibility.Visible;
+    }
+}
