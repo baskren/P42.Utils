38a877c113894877c7260f9c05d0fc8f1d09f750
Ben Askren <ben@buildcalc.com>
2020-10-14 21:31:45 -0400

    Added: - AssemblyExtensions.AssemblyFromResourceId to P42.Utils; - Added AssembliesToInclude to P42.Utils.Uno

--- P42.Utils.Uno/Settings.cs
+++ P42.Utils.Uno/Settings.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,14 +11,32 @@ namespace P42.Utils.Uno
 	{
 		public static Windows.UI.Xaml.Application Application { get; private set; }
 
-		public static void Init(Windows.UI.Xaml.Application application)
+        static List<Assembly> _assembliesToInclude;
+        public static List<Assembly> AssembliesToInclude
+        {
+            get
+            {
+                if (_assembliesToInclude == null)
+                {
+                    _assembliesToInclude = new List<Assembly>
+                    {
+                        typeof(P42.Utils.Debug).Assembly
+                    };
+                }
+                return _assembliesToInclude;
+            }
+        }
+
+        public static void Init(Windows.UI.Xaml.Application application)
 		{
 			Application = application;
 			P42.Utils.Environment.Init();
 			P42.Utils.Environment.PlatformPathLoader = PlatformPathLoader;
 			P42.Utils.DiskSpace.PlatformDiskSpace = new DiskSpace();
 			P42.Utils.Process.PlatformProcess = new P42.Utils.Uno.Process();
-		}
+            P42.Utils.Environment.EmbeddedResourceAssemblyResolver = AssemblyFromResourceId;
+
+        }
 
 		static void PlatformPathLoader()
 		{
@@ -38,5 +57,38 @@ namespace P42.Utils.Uno
 			P42.Utils.Environment.ApplicationCachePath = Windows.Storage.ApplicationData.Current.LocalCacheFolder.Path;
 			P42.Utils.Environment.TemporaryStoragePath = Windows.Storage.ApplicationData.Current.TemporaryFolder.Path;
 		}
-	}
+
+        public static Assembly AssemblyFromResourceId(string resourceId)
+        {
+            if (string.IsNullOrWhiteSpace(resourceId))
+                return null;
+            Assembly assembly = null;
+            var resourcePath = new List<string>(resourceId.Split('.'));
+            if (resourceId.Contains(".Resources."))
+            {
+                var resourceIndex = resourcePath.IndexOf("Resources");
+                var asmName = string.Join(".", resourcePath.GetRange(0, resourceIndex));
+                var appAsm = Windows.UI.Xaml.Application.Current.GetType().Assembly;  
+                if (appAsm?.GetName().Name == asmName)
+                    return appAsm;
+
+                foreach (var asm in AssembliesToInclude)
+                    if (asm?.GetName().Name == asmName)
+                        return asm;
+            }
+            if (assembly == null)
+            {
+                for (int i = resourcePath.Count - 1; i < 0; i--)
+                {
+                    var asmName = string.Join(".", resourcePath.GetRange(0, i));
+                    foreach (var asm in Settings.AssembliesToInclude)
+                        if (asm?.GetName().Name == asmName)
+                            return asm;
+                }
+            }
+            return assembly;
+        }
+
+
+    }
 }
--- P42.Utils/Extensions/AssemblyExtensions.cs
+++ P42.Utils/Extensions/AssemblyExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Reflection;
 namespace P42.Utils
