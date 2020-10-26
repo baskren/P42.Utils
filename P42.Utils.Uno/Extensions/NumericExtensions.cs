10fdfe8e0f281d5fca937b5730fb15fda589c6d6
Ben Askren <ben@buildcalc.com>
2020-10-26 12:48:51 -0400

    Put Color and Numeric extensions into P42.Utils.Uno (from Bc3.Forms)

---
+++ P42.Utils.Uno/Extensions/NumericExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P42.Utils.Uno
+{
+	public static class NumericExtensions
+	{
+		public static T Clamp<T>(this T self, T min, T max) where T : IComparable
+		{
+			if (Comparer<T>.Default.Compare(max, min) < 0)
+				return max;
+			else if (Comparer<T>.Default.Compare(self, min) < 0)
+				return min;
+			else if (Comparer<T>.Default.Compare(self, max) > 0)
+				return max;
+			return self;
+		}
+	}
+}
