93ca3d184ce0f4fb8669e40013fc7e22a975df7d
Ben Askren <ben@buildcalc.com>
2021-04-03 10:05:03 -0400

    Added StatusBarSize to AppWindow;

--- P42.Utils.Uno/AppWindow.cs
+++ P42.Utils.Uno/AppWindow.cs
@@ -1,18 +1,22 @@
 ﻿using Windows.Foundation;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+#if __ANDROID__
+using Android.Runtime;
+#endif
 
 namespace P42.Utils.Uno
 {
     public static class AppWindow
     {
-        public static Size Size()
+        public static Size Size(FrameworkElement element)
         {
             // Doens't work in Uno
             //var windowWidth = ((Frame)Windows.UI.Xaml.Window.Current.Content).ActualWidth;
             //var windowHeight = ((Frame)Windows.UI.Xaml.Window.Current.Content).ActualHeight;
 
             var windowWidth = Windows.UI.Xaml.Window.Current.Bounds.Width;
-            var windowHeight = Windows.UI.Xaml.Window.Current.Bounds.Height;
+            var windowHeight = Windows.UI.Xaml.Window.Current.Bounds.Height - StatusBarHeight(element);
             return new Size(windowWidth, windowHeight);
         }
 
@@ -22,5 +26,28 @@ namespace P42.Utils.Uno
         public static Frame CurrentFrame
             => (Frame)Windows.UI.Xaml.Window.Current.Content;
 
+        public static double StatusBarHeight(FrameworkElement element)
+        {
+#if __ANDROID__
+            if (element is Android.Views.View view)
+            {
+
+                var rect = new Android.Graphics.Rect();
+                var window = ((Android.App.Activity)view.Context).Window;
+                window.DecorView.GetWindowVisibleDisplayFrame(rect);
+
+                using var displayMetrics = new Android.Util.DisplayMetrics();
+
+                using var service = view.Context.GetSystemService(Android.Content.Context.WindowService);
+                using var windowManager = service?.JavaCast<Android.Views.IWindowManager>();
+
+                windowManager?.DefaultDisplay?.GetRealMetrics(displayMetrics);
+
+                var statusBarHeight = rect.Top / displayMetrics?.Density ?? 1;
+                return statusBarHeight;
+            }
+#endif
+            return 0;
+        }
     }
 }
