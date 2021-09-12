30e78f86bfbf32de84097ca3cfe48a0a57b8c124
Ben Askren <ben@buildcalc.com>
2021-09-12 16:27:39 -0400

    Added P42.Utils.MaterialIcons

---
+++ P42.Utils/MaterialIcons.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P42.Utils
+{
+    public static class MaterialIcons
+    {
+        public const string AssetPath = "ms-appx:///Assets/Fonts/MaterialIcons-Regular.ttf#Material Icons";
+
+        public const string Error_Filled = "\ue000";
+        public const string Error_Outline = "\ue001";
+        public const string Warning_Filled = "\u0002";
+        public const string Warning_Outline = "\uf083";
+
+        public const string Info = "\ue88e";
+        public const string Help_Filled = "\ue887";
+        public const string Help_Outline = "\ue8fd";
+        public const string Help_Square = "\uf1c0";
+        public const string Contact_Support = "\ue94c";
+
+        public const string Settings = "\ue8b8";
+        public const string Search = "\ue8b6";
+
+        public const string X_Outline = "\ue888";
+        public const string Trash_Outline = "\ue92e";
+
+        public const string Print = "\ue8ad";
+
+        public const string Unlocked = "\ue898";
+
+        public const string ArrowCircleRight = "\ueaaa";
+        public const string ArrowCircleLeft = "\ueaa7";
+        public const string ArrowCircleUp = "\ue7cd";
+        public const string ArrowCircleDown = "\uf181";
+
+        public const string Cancel = "\ue5c9";
+        public const string Clear = "\ue14c";
+        public const string AddCirlceOutline = "\ue148";
+        public const string Add = "\ue145";
+        public const string AddCircle = "\ue147";
+        public const string RemoveCircle = "\ue15c";
+
+        public const string ImportExport = "\ue0c3";
+
+        public const string ExpandMore = "\ue5cf";
+        public const string ExpandLess = "\ue5ce";
+        public const string ExpandCircleDown = "\ue5ce";
+
+        public const string UnfoldMore = "\ue5d7";
+        public const string UnfoldLess = "\ue5d6";
+
+        public const string MoreVert = "\ue5d4";
+        public const string MoreHorz = "\ue5d3";
+        public const string MoreHorzCircle = "\uef64";
+
+        public const string Circle = "\uef4a";
+        public const string BuildCircle = "\uef64";
+
+
+        public const string ArrowBack = "\ue5c4";
+        public const string ArrowForward = "\ue5c8";
+        public const string ArrowUp = "\ue5d8";
+        public const string ArrowDown = "\ue5db";
+
+        public const string North = "\uf1e0";
+        public const string NorthEast = "\uf1e1";
+        public const string East = "\uf1df";
+        public const string SouthEast = "\uf1e4";
+        public const string South = "\uf1e3";
+        public const string SouthWest = "\uf1e5";
+        public const string West = "\uf1e6";
+        public const string NorthWest = "\uf1e2";
+
+        public const string ChevronRight = "\ue5cc";
+        public const string ChevronLeft = "\ue5cb";
+        public const string LargeChevronRight = "\ue5e1";
+        public const string LargeChevronLeft = "\ue2ea";
+
+        public const string Refresh = "\ue5d5";
+        public const string Sync = "\ue627";
+        public const string Replay = "\ue042";
+        public const string SyncAlt = "\ue042";
+
+        public const string Backspace = "\ue14a";
+
+        public const string Folder = "\ue2c7";
+        public const string FolderShared = "\ue2c9";
+        public const string FolderUpload = "\ue9a3";
+
+        public const string Cloud = "\ue2bd";
+        public const string CloudCircle = "\ue2be";
+        public const string CloudUpload = "\ue2c3";
+        public const string CloudDownload = "\ue2c0";
+        public const string CloudDone = "\ue2bf";
+
+        public const string Filter1 = "\ue3d0";
+        public const string Filter2 = "\ue3d1";
+        public const string Filter3 = "\ue3d2";
+        public const string Filter4 = "\ue3d4";
+        public const string Filter5 = "\ue3d5";
+        public const string Filter6 = "\ue3d6";
+        public const string Filter7 = "\ue3d7";
+        public const string Filter8 = "\ue3d8";
+        public const string Filter9 = "\ue3d9";
+        public const string Filter9p = "\ue3da";
+        public const string FilterAdd = "\ue03c";
+        public const string FilterCheck = "\ue9b7";
+
+        public const string Calculate = "\uea5f";
+
+        public const string Check = "\ue5ca";
+        public const string CheckCircle = "\ue86c";
+        public const string CheckBox = "\ue834";
+        public const string CheckBoxBlank = "\ue835";
+        public const string CheckBoxIndeterminate = "\ue909";
+
+        public const string ToggleOn = "\ue9f6";
+        public const string ToggleOff = "\ue9f6";
+
+        public const string ListAddCheck = "\ue065";
+        public const string ListAdd = "\ue03b";
+        public const string ListAddCheckCircle = "\ue7e6";
+        public const string ListAddCircle = "\ue7e5";
+    }
+}
