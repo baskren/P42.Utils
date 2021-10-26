54ce8014c524db064f7d6f7147de219e99dd8f63
Ben Askren <ben@buildcalc.com>
2021-10-26 05:41:22 -0400

    DOCS (SystemColors) : Added some comments to P42.Utils.Uno.SystemColors

--- P42.Utils.Uno/Extensions/SystemColors.cs
+++ P42.Utils.Uno/Extensions/SystemColors.cs
@@ -7,59 +7,81 @@ using Windows.UI;
 
 namespace P42.Utils.Uno
 {
+    // XAML theme resources
+    // The XAML color ramp and theme-dependent brushes
+    // Light and Dark theme colors
+    // https://docstaging.z5.web.core.windows.net/aleader/toolkit-7/design/controls-and-patterns/xaml-theme-resources.html#the-xaml-color-ramp-and-theme-dependent-brushes
     public static class SystemColors
     {
+        // Light: 0xFFFFFFFF Dark: 0xFF000000
         public static Color AltHigh
             => ColorExtensions.AppColor("SystemAltHighColor");
 
+        // Light: 0x33FFFFFF Dark: 0x33000000
         public static Color AltLow
             => ColorExtensions.AppColor("SystemAltLowColor");
 
+        // Light: 0x99FFFFFF Dark: 0x99000000
         public static Color AltMedium
             => ColorExtensions.AppColor("SystemAltMediumColor");
 
+        // Light: 0xCCFFFFFF Dark: 0xCC000000
         public static Color AltMediumHigh
             => ColorExtensions.AppColor("SystemAltMediumHighColor");
 
+        // Light: 0x66FFFFFF Dark: 0x66000000
         public static Color AltMediumLow 
             => ColorExtensions.AppColor("SystemAltMediumLowColor");
 
+        // Light: 0xFF000000 Dark 0xFFFFFFFF
         public static Color BaseHigh 
             => ColorExtensions.AppColor("SystemBaseHighColor");
 
+        // Light: 0x33000000 Dark: 0x33FFFFFF
         public static Color BaseLow 
             => ColorExtensions.AppColor("SystemBaseLowColor");
 
+        // Light: 0x99000000 Dark: 0x99FFFFFF
         public static Color BaseMedium 
             => ColorExtensions.AppColor("SystemBaseMediumColor");
 
+        // Light: 0xCC000000 Dark: 0xCCFFFFFF
         public static Color BaseMediumHigh 
             => ColorExtensions.AppColor("SystemBaseMediumHighColor");
 
+        // Light: 0x66000000 Dark: 0x66FFFFFF
         public static Color BaseMediumLow 
             => ColorExtensions.AppColor("SystemBaseMediumLowColor");
 
+        // Light: 0xFF171717 Dark: 0xFFF2F2F2
         public static Color ChromeAltLow 
             => ColorExtensions.AppColor("SystemChromeAltLowColor");
 
+        // Light: 0xFF000000 Dark: 0xFF000000
         public static Color ChromeBlackHigh 
             => ColorExtensions.AppColor("SystemChromeBlackHighColor");
 
+        // Light: 33000000 Dark: 33000000
         public static Color ChromeBlackLow 
             => ColorExtensions.AppColor("SystemChromeBlackLowColor");
 
+        // Light: 66000000 Dark: 66000000
         public static Color ChromeBlackMediumLow 
             => ColorExtensions.AppColor("SystemChromeBlackMediumLowColor");
 
+        // Light: CC000000 Dark: CC000000
         public static Color ChromeBlackMedium 
             => ColorExtensions.AppColor("SystemChromeBlackMediumColor");
 
+        // Light: FFCCCCCC Dark: FF333333
         public static Color ChromeDisabledHigh 
             => ColorExtensions.AppColor("SystemChromeDisabledHighColor");
 
+        // Light: FF7A7A7A Dark: FF858585
         public static Color ChromeDisabledLow 
             => ColorExtensions.AppColor("SystemChromeDisabledLowColor");
 
+        // Light: FFCCCCCC Dark: FF767676
         public static Color ChromeHigh 
             => ColorExtensions.AppColor("SystemChromeHighColor");
 
