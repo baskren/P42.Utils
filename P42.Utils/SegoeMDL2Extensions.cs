a4df4b6121a5fef2b2bf43525e8a794ab0752f08
Ben Askren <ben@buildcalc.com>
2021-10-31 16:46:36 -0400

    FEAT (SegoeMDL2) : started

---
+++ P42.Utils/SegoeMDL2Extensions.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P42.Utils
+{
+    public static class SegoeMDL2
+    {
+        public const string AssetPath = "ms-appx:///Assets/Fonts/SegoeMDL2.ttf#Segoe MDL2 Assets";
+        public const string Info = "\uE946";
+        public const string InfoSolid = "\uF167";
+    }
+}
