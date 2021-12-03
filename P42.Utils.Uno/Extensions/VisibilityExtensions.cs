f4121b44e7d02f8cde3c898a89d8a85e953c6a99
Ben Askren <ben@buildcalc.com>
2021-12-03 17:00:44 -0500

    FEAT (Visibility) : Added NullEmptyWhiteSpaceVisibilityConverter (for binding)

--- P42.Utils.Uno/Extensions/VisibilityExtensions.cs
+++ P42.Utils.Uno/Extensions/VisibilityExtensions.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Uno.Extensions.Specialized;
 using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
 
 namespace P42.Utils.Uno
 {
@@ -21,5 +22,29 @@ namespace P42.Utils.Uno
 
         public static bool ToBool(this Visibility visibility)
             => visibility == Visibility.Visible;
+
+        static NullEmptyWhiteSpaceVisiblityConverter nullEmptyWhiteSpaceVisiblityConverter;
+        public static NullEmptyWhiteSpaceVisiblityConverter NullEmptyWhiteSpaceVisiblityConverter => nullEmptyWhiteSpaceVisiblityConverter = nullEmptyWhiteSpaceVisiblityConverter ?? new NullEmptyWhiteSpaceVisiblityConverter();
+    }
+
+    public class NullEmptyWhiteSpaceVisiblityConverter : IValueConverter
+    {
+        internal NullEmptyWhiteSpaceVisiblityConverter() { }
+
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value is bool tf)
+                return tf ? Visibility.Visible : Visibility.Collapsed;
+            if (value is string txt)
+                return string.IsNullOrWhiteSpace(txt) ? Visibility.Collapsed : Visibility.Visible;
+            if (value is null)
+                return Visibility.Collapsed;
+            return Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
