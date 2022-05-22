e0ad9266097b5c9be3fd4962df6f9ef8efe20563
Ben Askren <ben@buildcalc.com>
2022-05-22 16:24:12 -0400

    FEAT (AssemblyExtensions) : Added FindAssemblyForMultiResource, GetAssemblyByName, and GetAssemblies

--- P42.Utils/Extensions/AssemblyExtensions.cs
+++ P42.Utils/Extensions/AssemblyExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Reflection;
 namespace P42.Utils
 {
@@ -34,5 +35,46 @@ namespace P42.Utils
                 }
             }
         }
+
+        public static Assembly FindAssemblyForMultiResource(string resourceId, Assembly assembly = null)
+        {
+            if (assembly?.GetManifestResourceNames().Any(id => id.StartsWith(resourceId, StringComparison.Ordinal)) ?? false)
+                return assembly;
+            if (resourceId.IndexOf(".Resources.", StringComparison.Ordinal) is int index && index > 0)
+            {
+                var assemblyName = resourceId.Substring(0, index);
+                assembly = GetAssemblyByName(assemblyName);
+                if (assembly?.GetManifestResourceNames().Any(id => id.StartsWith(resourceId, StringComparison.Ordinal)) ?? false)
+                    return assembly;
+            }
+            assembly = System.Reflection.Assembly.GetExecutingAssembly();
+            if (assembly?.GetManifestResourceNames().Any(id => id.StartsWith(resourceId, StringComparison.Ordinal)) ?? false)
+                return assembly;
+            foreach (var assm in ReflectionExtensions.GetAssemblies())
+            {
+                if (!assm.IsDynamic && assm.GetManifestResourceNames().Any(id => id.StartsWith(resourceId, StringComparison.Ordinal)))
+                    return assm;
+            }
+            return null;
+        }
+
+        public static Assembly GetAssemblyByName(string name)
+        {
+            foreach (Assembly assembly in GetAssemblies())
+            {
+                if (assembly.GetName().Name == name)
+                {
+                    return assembly;
+                }
+            }
+
+            return null;
+        }
+
+        public static List<Assembly> GetAssemblies()
+        {
+            return AppDomain.CurrentDomain.GetAssemblies()?.ToList();
+        }
+
     }
 }
