1cb927f09f9a9464381c5c9364380d50e77c6708
baskren <ben@buildcalc.com>
2025-04-09 15:16:03 -0400

    FEAT : P42.Utils.Uno.ApplicationExtensions.CloseConsoleWindow();

--- P42.Utils.Uno/Extensions/ApplicationExtensions.cs
+++ P42.Utils.Uno/Extensions/ApplicationExtensions.cs
@@ -9,16 +9,36 @@ using Microsoft.UI.Xaml;
 namespace P42.Utils.Uno;
 public static  class ApplicationExtensions
 {
-    public static bool CreateConsoleWindow(this Application app)
-    {
-        return AllocConsole();
-    }
+    /// <summary>
+    /// Create a window that shows console ouput (only works in WinAppSdk)
+    /// </summary>
+    /// <param name="app"></param>
+    /// <returns></returns>
+    public static bool OpenConsoleWindow(this Application app)
+        => AllocConsole();
+
+    /// <summary>
+    /// Close a window that shows console ouput (only works in WinAppSdk)
+    /// </summary>
+    /// <param name="app"></param>
+    /// <returns></returns>
+    public static bool CloseConsoleWindow(this Application app)
+        => FreeConsole();
+
+
 
 #if WINDOWS
     [DllImport("kernel32.dll", SetLastError = true, ExactSpelling = true)]
     [return: MarshalAs(UnmanagedType.Bool)]
     private static extern bool AllocConsole();
+
+    [System.Runtime.InteropServices.DllImport("kernel32.dll")]
+    private static extern bool FreeConsole();
+
 #else
     private static bool AllocConsole() => false;
+
+    private static bool FreeConsole() => false;
 #endif
+
 }
